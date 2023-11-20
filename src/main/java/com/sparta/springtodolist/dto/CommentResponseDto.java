1f913aa846620944a6369f30010b1d2d15b7cb91
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2023-11-20 12:41:05 +0900

    feat : 1차 완성본

--- src/main/java/com/sparta/springtodolist/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/springtodolist/dto/CommentResponseDto.java
@@ -5,12 +5,12 @@ import lombok.Getter;
 
 @Getter
 public class CommentResponseDto {
-
+    private final Long id;
     private final String username;
     private final String detail;
 
     public CommentResponseDto(Comment comment) {
-
+        this.id = comment.getId();
         this.username = comment.getUser().getUsername();
         this.detail = comment.getDetail();
 
